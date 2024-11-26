005ef17aeb85d45d67f4d5a0bed91a28668a7fe3
Ziyang Qiu <ziyangqiu@outlook.com>
2024-11-26 09:26:20 -0500

    make plt 2x2

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ def drop_zscore(data, thresh = 10):
   return data[np.abs(z_scores) <= thresh]
 
 # Exoplanet Attributes: IBHL Collaborative Project
-
+fig, ax = plt.subplots(2, 2)
 df = pd.read_csv('exoplanets.csv')
+
 year_len = df['Period (days)'].astype(str).str.replace(r"±[\d,.]+", "", regex=True)
 year_len = year_len[~year_len.str.contains(r'^[\d,.]+\+[\d,.]+−[\d,.]+$')].astype(np.float32).dropna()
 year_len = drop_zscore(drop_zscore(drop_zscore(drop_zscore(drop_zscore(year_len, 1), 1), 1), 1), 1)
 
-# Bar Graph
-fig, ax = plt.subplots(2, 1)
-ax[0].hist(year_len, bins=50)
-ax[0].set_title('Length of Year')
+ax[0][0].hist(year_len, bins=50)
+ax[0][0].set_title('Length of Year')
+ax[0][0].set_xlabel('Period (days)')
+ax[0][0].set_ylabel('Number of exoplanets')
+ax[0][0].grid(axis='y')
 
-ax[0].set_xlabel('Period (days)')
-ax[0].set_ylabel('Number of exoplanets')
-ax[0].grid(axis='y')
 
 fig.tight_layout()
 fig.show()
